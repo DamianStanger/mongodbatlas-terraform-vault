a578eeabfc34c24d2ab03d8e48a9a8b0be8bf58f
Damian Stanger <damian.stanger@lexisnexisrisk.com>
2022-05-25 09:42:19 +0100

    tidyup and manual merge from provider3 branch

--- provider1.tf
+++ provider1.tf
@@ -1,4 +1,4 @@
 provider "mongodbatlas" {
-  public_key = var.mongodbatlas_temp_public_key
+  public_key   = var.mongodbatlas_temp_public_key
   private_key  = var.mongodbatlas_temp_private_key
 }
--- versions.tf
+++ versions.tf
@@ -3,8 +3,12 @@ terraform {
 
   required_providers {
     mongodbatlas = {
-      source = "mongodb/mongodbatlas"
+      source  = "mongodb/mongodbatlas"
       version = "1.3.1"
     }
+    # vault   = {
+    #   source  = "hashicorp/vault"
+    #   version = "3.6.0"
+    # }
   }
 }
